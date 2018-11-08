981ada55209efe2d4284267423218c071326eed3
soyoungna <hos05360@naver.com>
2018-11-08 23:01:56 +0900

    bingo_1108

---
+++ Bingo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	
+	for(i=0;i<N;i++)
+	{
+		printf("숫자를입력하세요: ")
+	}
+	
+	return 0;
+}
+int void initiate_bingo{
+	
+}
+
