d3d47dcc4cf194747d76dc9154e27ea8bcae41b0
soyoungna <hos05360@naver.com>
2018-11-14 23:52:01 +0900

    11/14bingo

--- Bingo.c
+++ Bingo.c
@@ -1,42 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 5
+#define M 3 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int initiate_bingo();
-	int print_bingo();
+	void initiate_bingo();
+	void print_bingo();
 	int get_number_byMe();
 	int get_number_byCom();
 	int process_bingo();
 	int count_bingo();
 
     initiate_bingo();
+    
+    print_bingo();
+    
 	return 0;
 }
-int initiate_bingo()
-{
-	int N;
-	int get_num[N][N];
-	int i=0;
-	int j=0;
+void initiate_bingo() //난수 이용해서 빙고판 만들기  
+{ 
+   
+	int bingo_num[N*N];
+	int i;
+    int temp,sour,dest;
+    
+	srand((unsigned int)time(NULL));
 	
-	printf("원하는 빙고의 줄을 말하시오 : ");
-	scanf("%d",&N);
-	
-	printf("빙고안에 숫자를 입력하세요: ");
-	for (i=0;i<N;i++)	
-	    for (j=0;j<N;j++){
-	    	scanf("%d%d\n",&get_num[i][j]); //이것은 내가 만드는 빙고테이블에 들어가야함 잘못됨, 빙고테이블은 어떻게만들지?
+	for (i=0;i<=N*N;i++){
+		
+		bingo_num[i]=i+1;
+	}// 빙고 배열에 1부터 N*N까지 넣기
 	 
-	    	
-		}
+	    
+	for(i=0;i<N*N;i++){
+		sour=rand() % N*N+1;
+		dest=rand() % N*N+1;
+	
+		temp = bingo_num[sour];
+        bingo_num[sour] = bingo_num[dest];
+        bingo_num[dest] = temp;
+
+	}//빙고배열판 섞기
+	 
+		
 	
 	 return 0;
 	}
 
-
+void print_bingo()
+{
+	int x,y;
+    int bingo_array[N][N];
+	
+	for(x=0;x<N;x++){
+		for(y=0;y<N;y++){
+			
+			printf(" %d ",bingo_array[x][y]);
+		}
+		printf("\n");
+	}
+	
+}
 	
 
